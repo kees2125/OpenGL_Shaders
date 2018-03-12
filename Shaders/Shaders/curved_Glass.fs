ec9d0d01f460c516d13493eb326a47564ea3f317
Kees Zijlmans <kees2125@gmail.com>
2018-03-12 17:05:47 +0100

    test glass shader

--- Shaders/Shaders/curved_Glass.fs
+++ Shaders/Shaders/curved_Glass.fs
@@ -7,6 +7,36 @@ uniform vec3 camaraPosision;
 
 void main()
 {
+//Pixel* dst = m_Surface->GetBuffer() + bx + by * pitchscreen;
+//	for ( int x = 128; x > 0; x-- )
+//	{
+//		float lol =cheapsin( 0.025f * (float)(x - 50));
+//		float dx = (float)(x - 64);
+//		int ymax = (int)(abs(sqrt(4096-(dx*dx))));
+//		int ymin = -ymax;
+//		ymin++;
+	
+//		//for ( int y = 128; y > 0; y-- )
+//		for(int y = ymax;y>ymin;y--)
+//		{
+			
+//			int y1 = y+64;
+//			int dist = (int)sqrt( dx * dx + y * y );
+//		    int xoffs = (int)(((dist >> 1) + 10) * lol);
+//			int yoffs = (int)(((dist >> 1) + 10) * cheapsin( 0.025f * (float)(y1 - 50) ) );
+//			int u1 = ((bx + x) - 4 * xoffs) % SCRWIDTH;
+//			int v1 = ((by + y1) - 4 * yoffs) % SCRHEIGHT;
+//			int u2 = ((bx + x) + 2 * xoffs) % SCRWIDTH;
+//			int v2 = ((by + y1) + 2 * yoffs) % SCRHEIGHT;
+//			Pixel refl = src[u1 + v1 * pitch];
+//			Pixel refr = src[u2 + v2 * pitch];
+//			int reflscale = (int)(63.0f - 0.015f * ( -dist ++) * ( -dist ++));
+//			int refrscale = (int)(0.015f * (-dist ++) * (- dist ++));
+//			dst[x + y * m_Surface->GetPitch()] = ScaleColor( refl, 31 - (reflscale >> 1) ) + ScaleColor( refr, 63 - refrscale );
+			
+//		}
+//	}
+
             vec3 normalDirection;
             vec3 viewDirection;
             mat4 ModelMatrix = modelMatrix;
